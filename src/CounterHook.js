b58c842946f8dfbeebdbfd4ca970a2a98f4cebc1
Matthew Lock <matthew_lock20@hotmail.com>
2020-09-28 12:44:09 +0100

    added guardian api

--- src/CounterHook.js
+++ src/CounterHook.js
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const CounterHook = () => {
   const[counter, setCounter] = useState(0)
+  const[items, setItems] = useState([])
 
   const counterAddOne = () =>setCounter(counter +1)
+
+  useEffect(() => {
+    fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?q=coronavirus")
+      .then(response => response.json())
+      .then(data => 
+        setItems(data["response"]["results"]))
+      }, [])
 
   return(
     <div>
       {counter}
       <button onClick={counterAddOne}>Another One</button>
+      {items.map(item => <p>{item["webTitle"]}</p>)}
     </div>
   )
 }
